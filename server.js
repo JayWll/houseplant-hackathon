ce75066657dced5c471d27c3fae053de4ae2dc19
Glitch (hello-express) <none>
2019-12-16 19:05:18 +0000

    🍕✊ Checkpoint ./server.js:7435365/248

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ app.get("/", function(request, response) {
   response.sendFile(__dirname + "/views/index.html");
 });
 
+app.get("/newreading", function(request, response) {
+  response.status(200).send('GlitchApp Success').end();
+});
+
 // listen for requests :)
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
