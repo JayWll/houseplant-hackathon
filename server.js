e99e5d98f61a61f37e543f770e15ff110d873e66
Glitch (hello-express) <none>
2019-12-16 20:50:56 +0000

    💖🍄 Checkpoint ./server.js:7435365/776

--- server.js
+++ server.js
@@ -17,20 +17,6 @@ app.get("/", function(request, response) {
 });
 
 app.get("/newreading", function(request, response) {
-  var message = {}
-  message.text = 'Value received: ' + request.query.v;
-  
-  var options = {
-    host: 'chat.googleapis.com',
-    method: 'POST',
-    headers: {
-      'Content-type': 'application/json'
-    }
-  };
-
-  options.path = '/v1/spaces/AAAAUtfnG10/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=GWdhpd0zwNfelfUXVLtH-tikPRuiOlEGMcYQyePCvqU%3D';
-  sendRequest(options, message);
-  
   response.status(200).send('GlitchApp Success').end();
 });
 
@@ -38,21 +24,3 @@ app.get("/newreading", function(request, response) {
 const listener = app.listen(process.env.PORT, function() {
   console.log("Your app is listening on port " + listener.address().port);
 });
-
-function sendRequest(opt, msg) {
-  var https = require('https');
-  var request = https.request(opt, function(response) {
-    var responseString = "";
-
-    response.on('data', function(data) {
-      responseString += data;
-    });
-
-    response.on('end', function() {
-      console.log(responseString);
-    });
-  });
-
-  request.write(JSON.stringify(msg));
-  request.end();
-}
