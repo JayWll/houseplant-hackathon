555ae80afc65587593d8ffe58f371bdf63d541f5
JayWll <jaywll@outlook.com>
2020-04-11 12:29:56 -0600

    Simplifying database connection options

--- nodeapp/app.js
+++ nodeapp/app.js
@@ -2,20 +2,11 @@
 const express = require('express');
 const Sequelize = require('sequelize');
 const Op = Sequelize.Op
-//const request = require('request');      // TEMPORARY! Used for import function.
 const app = express();
 
 // Setup database, using credentials set in .env
 var sequelize = new Sequelize('database', process.env.DB_USER, process.env.DB_PASS, {
-  host: '0.0.0.0',
   dialect: 'sqlite',
-  pool: {
-    max: 5,
-    min: 0,
-    idle: 10000
-  },
-    // Security note: the database is saved to the file `database.sqlite` on the local filesystem. It's deliberately placed in the `.data` directory
-    // which doesn't get copied if someone remixes the project.
   storage: '.data/database.sqlite'
 });
 var Readings;
