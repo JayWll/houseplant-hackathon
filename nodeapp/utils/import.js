2643bf44105584bb3cca6cfb41f2ff1d8500ab59
JayWll <jaywll@outlook.com>
2020-04-25 15:30:06 -0600

    Adding functionality to import settings from production

--- nodeapp/utils/import.js
+++ nodeapp/utils/import.js
@@ -1,14 +1,15 @@
-const db = require('../db/db')
+const db = require('../src/db')
 const request = require('request')
 const path = require('path')
 require('dotenv').config({path: path.join(__dirname, '../../.env')})
 
 // The URL from which to export production data
-const url = 'https://jasonsplant.glitch.me/exportall'
+const readingsurl = 'https://jasonsplant.glitch.me/exportall'
+const settingsurl = 'https://jasonsplant.glitch.me/showsettings'
 
-// Get data from the production server
+// Get readings from the production server
 request({
-  url,
+  url: readingsurl,
   headers: {
     'export-key': process.env.SECRET
   },
@@ -17,14 +18,31 @@ request({
   if (error) return console.log(error)
   if (response.statusCode != 200) return console.log('Unsuccessful, as indicated by HTTP status')
 
-  console.log(response.body.length + ' items retrieved')
-
-  // Sync the database table
-  await db.Settings.sync()
+  console.log(response.body.length + ' readings retrieved')
 
   // Remove existing database items
   await db.Readings.destroy({truncate: true})
 
   // Add new data to the database
   await db.Readings.bulkCreate(response.body)
+});
+
+// Get settings from the production server
+request({
+  url: settingsurl,
+  headers: {
+    'export-key': process.env.SECRET
+  },
+  json: true
+}, async(error, response) => {
+  if (error) return console.log(error)
+  if (response.statusCode != 200) return console.log('Unsuccessful, as indicated by HTTP status')
+
+  console.log(response.body.length + ' settings retrieved')
+
+  // Remove existing database items
+  await db.Settings.destroy({truncate: true})
+
+  // Add new data to the database
+  await db.Settings.bulkCreate(response.body)
 });
