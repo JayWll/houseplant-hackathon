b2cafddf233f6cbdb3c2bc9bdad7cb6d8dc9983f
JayWll <jaywll@outlook.com>
2020-04-25 15:07:01 -0600

    Move alert code to its own module

--- nodeapp/src/alert.js
+++ nodeapp/src/alert.js
@@ -4,41 +4,46 @@ require('dotenv').config({path: path.join(__dirname, '../../.env')})
 
 const alert = (type) => {
   console.log('Alert triggered for type: ' + type)
+  const tweet = false
 
   switch(type) {
     // Water is needed
     case 'wantwater':
-      const tweet = [
+      tweet = [
         'I\'m getting pretty thirsty over here, @JayWll',
         'So. Very. Dry. You know I\'m a succulent not a cactus, right @JayWll? 🌵',
         'Hey @JayWll, water me. 🥛',
         'They say a man shall not live by bread alone, and a succulent won\'t live if you don\'t WATER ME @JAYWLL!!1! 🍞🚰💦',
         '@JayWll seriously bro. A little water. That\'s all I ask.'
       ]
-      break;
+      break
 
     // Water has been received
     case 'gotwater':
-      const tweet = [
+      tweet = [
         'Thanks to whoever watered me just now!! So moist.',
         'So refreshing! Thank you, mysterious stranger, for the life-giving liquidy sustenance.',
         'Whoever watered me (I\'m kind of guessing it was you, @Katheryn_mur), know that I appreciate it!'
       ]
-      break;
-    }
-
-    // Define options for the twitter API
-    const msg = new twit({
-      consumer_key: process.env.CONSUMER_KEY,
-      consumer_secret: process.env.CONSUMER_SECRET,
-      access_token: process.env.ACCESS_TOKEN,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    })
-
-    // Post a tweet
-    //msg.post('statuses/update', { status: tweet[Math.floor(Math.random() * tweet.length)] })
-    console.log(tweet[Math.floor(Math.random() * tweet.length)])
+      break
   }
+
+  if (!tweet) {
+    console.log('nothing to send')
+    return
+  }
+
+  // Define options for the twitter API
+  const msg = new twit({
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token: process.env.ACCESS_TOKEN,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
+  })
+
+  // Post tweet
+  //msg.post('statuses/update', { status: tweet[Math.floor(Math.random() * tweet.length)] })
+  console.log(tweet[Math.floor(Math.random() * tweet.length)])
 }
 
 module.exports = alert
